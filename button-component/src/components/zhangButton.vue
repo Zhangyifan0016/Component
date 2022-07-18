6fd08b68f9395056f087bbace893c8bfe99337bc
Zhangyifan0016 <z351072259@163.com>
2022-07-18 21:08:48 +0800

    完成了封装button按钮

---
+++ button-component/src/components/zhangButton.vue
@@ -0,0 +1,77 @@
+<template>
+  <button
+    class="zhang-button"
+    @click="change"
+    :disabled="disabled || loading"
+    :class="[theme, isRound, isBorder, isSize, isBlock]"
+  >
+    <span>
+      <i v-if="loading" class="iconfont icon-prefix icon-loading"></i>
+      <i v-if="prefix" class="iconfont icon-prefix" :class="iconPrefix"></i>
+      <slot></slot>
+      <i v-if="suffix" class="iconfont icon-prefix" :class="iconSuffix"></i>
+    </span>
+  </button>
+</template>
+<script>
+export default {
+  name: 'zhangButton',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    size: {
+      type: String,
+      default: ''
+    },
+    prefix: {
+      type: String,
+      default: ''
+    },
+    suffix: {
+      type: String,
+      default: ''
+    },
+    round: Boolean,
+    border: Boolean,
+    block: Boolean,
+    disabled: Boolean,
+    loading: Boolean
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    theme() {
+      return this.type ? `zhang-button-${this.type}` : ''
+    },
+    isRound() {
+      return this.round ? 'is-round' : ''
+    },
+    isBorder() {
+      return this.border ? 'is-border' : ''
+    },
+    isSize() {
+      return this.size ? `zhang-button-${this.size}` : ''
+    },
+    isBlock() {
+      return this.block ? 'zhang-button-block' : ''
+    },
+    iconPrefix() {
+      return this.prefix ? `icon-${this.prefix}` : ''
+    },
+    iconSuffix() {
+      return this.suffix ? `icon-${this.suffix}` : ''
+    }
+  },
+  methods: {
+    change() {
+      this.$emit('click')
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+@import './zhangButton.scss';
+</style>
---
+++ button-component/src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <zhangButton size="mini" border round type="primary">默认按钮 </zhangButton>
+    <zhangButton
+      :disabled="false"
+      :loading="loadingStatus"
+      @click="handleSubmit"
+      type="success"
+      size="medium"
+      >成功按钮</zhangButton
+    >
+    <zhangButton disabled type="danger" size="small">危险按钮</zhangButton>
+    <zhangButton prefix="browse" suffix="eye-close" type="warning" block>
+      警告按钮
+    </zhangButton>
+  </div>
+</template>
+
+<script>
+import zhangButton from '../components/zhangButton'
+export default {
+  name: 'HomeView',
+  data() {
+    return {
+      loadingStatus: false
+    }
+  },
+  components: {
+    zhangButton
+  },
+  methods: {
+    handleSubmit() {
+      this.loadingStatus = true
+      setTimeout(() => {
+        this.loadingStatus = false
+      }, 3000)
+    }
+  }
+}
+</script>
